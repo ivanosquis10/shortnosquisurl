bfe86a59cf729002b5ef05667b573fcb60cd9f71
ivanosquis10 <ivanosquis10.12@gmail.com>
2024-01-15 15:10:53 -0400

    [fix]: changed the url view, no full, just short url

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Files } from "lucide-react"
+import { Files, LinkIcon } from "lucide-react"
 import Link from "next/link"
 import { toast } from "sonner"
 
@@ -23,18 +23,18 @@ export function Result({ newUrl }: { newUrl: string }) {
       <CardContent>
         <div className="flex items-center justify-between">
           <Link
-            className="font-medium transition-all hover:underline"
+            className="flex items-center text-lg font-semibold transition-all hover:underline"
             href={newUrl}
             target="_blank"
             title="Go to your shortened URL"
           >
-            {`${defaultUrl}/${newUrl}`}
+            <LinkIcon className="mr-2 h-5 w-5 text-emerald-500" strokeWidth={3} />
+            {`${newUrl}.com`}
           </Link>
           <Button
             className="flex gap-1 font-bold"
             size="sm"
             title="Copy your shortened URL"
-            // variant="ghost"
             onClick={copyUrl}
           >
             Copy
