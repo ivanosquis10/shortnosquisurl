bd459f2b78a381e8febec4c957cd1fc2b108c61d
ivanosquis10 <ivanosquis10.12@gmail.com>
2024-02-05 13:26:45 -0400

    [add]: new nav header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,32 @@
+import { GithubIcon } from "lucide-react"
 import Link from "next/link"
 
 export function Header() {
   return (
     <header className="container absolute top-0 m-auto flex w-full items-center justify-between py-2">
       <p className="bg-white text-sm opacity-70">© {new Date().getFullYear()} YokoCut</p>
-      <Link
-        className="bg-white text-sm font-medium underline underline-offset-4 opacity-70"
-        href="https://github.com/ivanosquis10"
-        rel="noreferrer"
-        target="_blank"
-      >
-        Built by Ivan😻
-      </Link>
+      <ul className="flex items-center gap-2">
+        <li>
+          <Link
+            className="bg-white text-sm font-medium underline underline-offset-4 opacity-70"
+            href="https://github.com/ivanosquis10"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Built by Ivan😻
+          </Link>
+        </li>
+        <li className="rounded-md bg-white">
+          <Link
+            href="https://github.com/ivanosquis10/shortnosquisurl"
+            rel="noreferrer"
+            target="_blank"
+            title="Link of the project on GitHub"
+          >
+            <GithubIcon className="h-6 w-6" />
+          </Link>
+        </li>
+      </ul>
     </header>
   )
 }
